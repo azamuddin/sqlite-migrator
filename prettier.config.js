f2e927a924133855f61548d3716b90d95889cd21
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2023-07-08 19:14:07 +0700

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Options} */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+}
+
+module.exports = config
